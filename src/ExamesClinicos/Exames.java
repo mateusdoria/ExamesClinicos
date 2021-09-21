b88f66f18d3236395c18987d8d30c1c64f202586
MateusDoRia <mateus.doria@outlook.com>
2021-09-21 20:50:29 -0300

    att

---
+++ src/ExamesClinicos/Exames.java
@@ -0,0 +1,28 @@
+package ExamesClinicos;
+
+import java.io.Serializable;
+
+public abstract class Exames implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private   String nome_do_medico;
+    private   String convenio;
+    private   String nome_do_paciente;
+    protected String especie;
+
+    public Exames(String nome_do_medico, String convenio, String nome_do_paciente) {
+        this.nome_do_medico = nome_do_medico;
+        this.convenio = convenio;
+        this.nome_do_paciente = nome_do_paciente;
+    }
+    public String toString() {
+        String retorno = "";
+        retorno += "Nome do Médico: "     + this.nome_do_medico     + "\n";
+        retorno += "Convenio: "    + this.convenio    + " \n";
+        retorno += "Nome do Paciente: "     + this.nome_do_paciente     + "\n";
+        retorno += "Especie: "  + this.especie  + "\n";
+        retorno += "Barulho: "  + soar()        + "\n";
+        return retorno;
+    }
+    public abstract String soar();
+}
